13d963496c4899053843ea9466f3d9e6d612c197
diac <nikolai.gladkikh@gmail.com>
2023-06-14 19:02:21 +0300

    [#185401822] Domain module: Convert IdeaReview to a JPA entity

--- domain/src/main/java/com/diac/ydeas/domain/model/IdeaReview.java
+++ domain/src/main/java/com/diac/ydeas/domain/model/IdeaReview.java
@@ -1,12 +1,15 @@
 package com.diac.ydeas.domain.model;
 
 import com.diac.ydeas.domain.enumeration.IdeaStatus;
+import jakarta.persistence.*;
 import jakarta.validation.constraints.NotNull;
 import lombok.*;
 
 /**
  * Модель данных "Рассмотрение идеи"
  */
+@Entity
+@Table(name = "idea_review")
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
@@ -17,18 +20,25 @@ public class IdeaReview {
     /**
      * Идея
      */
+    @Id
     @EqualsAndHashCode.Include
+    private int ideaId;
+
+    @OneToOne
+    @JoinColumn(name = "id", referencedColumnName = "idea_id")
     private Idea idea;
 
     /**
      * Пользователь-эксперт, рассмотревший идею
      */
-    @NotNull(message = "Idea reviewer is required")
-    private User reviewer;
+    @NotNull(message = "Idea reviewer ID is required")
+    @Column(name = "reviewer_user_id")
+    private Integer reviewerUserId;
 
     /**
      * Статус идеи после рассмотрения
      */
+    @Enumerated(EnumType.STRING)
     @NotNull(message = "Idea status is required")
     private IdeaStatus ideaStatus;
 }
