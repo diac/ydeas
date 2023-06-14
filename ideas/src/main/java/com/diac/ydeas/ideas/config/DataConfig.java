1670977ee9d74ef84437bfdf6b4d3a912f0cd9dd
diac <nikolai.gladkikh@gmail.com>
2023-06-14 17:31:28 +0300

    [#185401815] Ideas module: Add DataConfig

---
+++ ideas/src/main/java/com/diac/ydeas/ideas/config/DataConfig.java
@@ -0,0 +1,38 @@
+package com.diac.ydeas.ideas.config;
+
+import jakarta.persistence.EntityManagerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.PlatformTransactionManager;
+
+import javax.sql.DataSource;
+
+@Configuration
+@EnableJpaRepositories("com.diac.ydeas.ideas.repository")
+public class DataConfig {
+
+    private static final String DOMAIN_MODEL_PACKAGE_LOCATION = "com.diac.ydeas.domain.model";
+
+    @Primary
+    @Bean
+    public LocalContainerEntityManagerFactoryBean entityManagerFactory(DataSource ds) {
+        HibernateJpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
+        LocalContainerEntityManagerFactoryBean factory = new LocalContainerEntityManagerFactoryBean();
+        factory.setJpaVendorAdapter(vendorAdapter);
+        factory.setPackagesToScan(DOMAIN_MODEL_PACKAGE_LOCATION);
+        factory.setDataSource(ds);
+        return factory;
+    }
+
+    @Bean
+    public PlatformTransactionManager transactionManager(EntityManagerFactory entityManagerFactory) {
+        JpaTransactionManager txManager = new JpaTransactionManager();
+        txManager.setEntityManagerFactory(entityManagerFactory);
+        return txManager;
+    }
+}
