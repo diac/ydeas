506cd38f5f09e9228d7b72238298ea7a856d4a8d
diac <nikolai.gladkikh@gmail.com>
2023-06-14 18:09:01 +0300

    [#185401815] Ideas module: Create "idea_review" table

---
+++ ideas/src/main/resources/db/scripts/003_ddl_create_table_idea_review.sql
@@ -0,0 +1,10 @@
+CREATE TABLE idea_review (
+    idea_id INTEGER NOT NULL PRIMARY KEY REFERENCES idea(id),
+    reviewer_user_id INTEGER NOT NULL,
+    idea_status VARCHAR NOT NULL
+);
+
+COMMENT ON TABLE idea_review IS 'Рассмотрение идеи';
+COMMENT ON COLUMN idea_review.idea_id IS 'Идентификатор идеи (PK + FK)';
+COMMENT ON COLUMN idea_review.reviewer_user_id IS 'Идентификатор пользователя-эксперта, рассмотревшего идею';
+COMMENT ON COLUMN idea_review.idea_status IS 'Статус идеи после рассмотрения';
