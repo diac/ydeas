baa68745d4a3326ab8f235174464d80e296f2cbd
diac <nikolai.gladkikh@gmail.com>
2023-06-14 12:58:49 +0300

    [#185401819] Domain module: Add Rate enumeration

---
+++ domain/src/main/java/com/diac/ydeas/domain/enumeration/Rate.java
@@ -0,0 +1,17 @@
+package com.diac.ydeas.domain.enumeration;
+
+/**
+ * Перечисление типов оценок
+ */
+public enum Rate {
+
+    /**
+     * Оценка "Нравится"
+     */
+    LIKE,
+
+    /**
+     * Оценка "Не нравится"
+     */
+    DISLIKE
+}
