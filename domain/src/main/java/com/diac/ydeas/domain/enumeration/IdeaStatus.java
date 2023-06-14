5f9344c11847deeaaa8dadacfa749f430141a892
diac <nikolai.gladkikh@gmail.com>
2023-06-14 13:36:36 +0300

    [#185401819] Domain module: Add IdeaStatus enumeration

---
+++ domain/src/main/java/com/diac/ydeas/domain/enumeration/IdeaStatus.java
@@ -0,0 +1,17 @@
+package com.diac.ydeas.domain.enumeration;
+
+/**
+ * Перечисление "Статус идеи"
+ */
+public enum IdeaStatus {
+
+    /**
+     * Идея утверждена
+     */
+    APPROVED,
+
+    /**
+     * Идея отклонена
+     */
+    DECLINED
+}
