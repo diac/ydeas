077374e6c8344adc659f28367d2cfb383990a332
diac <nikolai.gladkikh@gmail.com>
2023-06-18 14:10:45 +0300

    [#185413753] Ideas module: IdeaRatingController

---
+++ ideas/src/main/java/com/diac/ydeas/ideas/controller/IdeaRatingController.java
@@ -0,0 +1,39 @@
+package com.diac.ydeas.ideas.controller;
+
+import com.diac.ydeas.domain.model.IdeaRating;
+import com.diac.ydeas.ideas.service.IdeaRatingService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+/**
+ * Контроллер, реализующий доступ к объектам модели IdeaRating
+ */
+@RestController
+@AllArgsConstructor
+@RequestMapping("/idea_rating")
+public class IdeaRatingController {
+
+    /**
+     * Сервис для работы с объектами IdeaRating
+     */
+    private final IdeaRatingService ideaRatingService;
+
+    /**
+     * Получить рейтинговый список идей
+     *
+     * @return Тело ответа с рейтинговым списком идей и статусом OK
+     */
+    @GetMapping("")
+    public ResponseEntity<List<IdeaRating>> rating() {
+        return new ResponseEntity<>(
+                ideaRatingService.rating(),
+                HttpStatus.OK
+        );
+    }
+}
