070645eaf940eba8f1ebb0a6b005192d44cf01c0
diac <nikolai.gladkikh@gmail.com>
2023-08-07 15:35:19 +0300

    [#185432800] Domain: Add values in Authority enumeration

--- domain/src/main/java/com/diac/ydeas/domain/enumeration/Authority.java
+++ domain/src/main/java/com/diac/ydeas/domain/enumeration/Authority.java
@@ -5,4 +5,23 @@ package com.diac.ydeas.domain.enumeration;
  */
 public enum Authority {
 
+    /**
+     * Чтение идей
+     */
+    IDEAS_READ,
+
+    /**
+     * Запись идей
+     */
+    IDEAS_WRITE,
+
+    /**
+     * Голосование за идеи
+     */
+    IDEAS_VOTE,
+
+    /**
+     * Одобрение идей
+     */
+    IDEAS_APPROVE
 }
