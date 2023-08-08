a1e969203d78c1b2ab5bd5a5940788ded95c783a
diac <nikolai.gladkikh@gmail.com>
2023-08-08 16:11:05 +0300

    [#185771783] Ideas service: Enable Testcontainers in AbstractRepositoryIntegrationTest

--- ideas/src/test/java/com/diac/ydeas/ideas/repository/AbstractRepositoryIntegrationTest.java
+++ ideas/src/test/java/com/diac/ydeas/ideas/repository/AbstractRepositoryIntegrationTest.java
@@ -1,12 +1,20 @@
 package com.diac.ydeas.ideas.repository;
 
 import com.diac.ydeas.ideas.config.DataConfig;
+import com.diac.ydeas.ideas.repository.container.PostgreSQLTestContainersExtension;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ContextConfiguration;
 
 @DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 @ContextConfiguration(classes = {
         DataConfig.class
 })
+@ExtendWith(PostgreSQLTestContainersExtension.class)
+@DirtiesContext
 public abstract class AbstractRepositoryIntegrationTest {
+
 }
