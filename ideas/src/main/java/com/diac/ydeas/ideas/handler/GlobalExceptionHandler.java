54c6e59fb844b1f264720b928bce2c3b2422f2fd
diac <nikolai.gladkikh@gmail.com>
2023-08-09 16:47:28 +0300

    [#185785474] Ideas module: Handle ResourceOwnershipViolationException exceptions in GlobalExceptionHandler and translate them to "403 Forbidden" HTTP status

--- ideas/src/main/java/com/diac/ydeas/ideas/handler/GlobalExceptionHandler.java
+++ ideas/src/main/java/com/diac/ydeas/ideas/handler/GlobalExceptionHandler.java
@@ -2,13 +2,14 @@ package com.diac.ydeas.ideas.handler;
 
 import com.diac.ydeas.domain.exception.ResourceConstraintViolationException;
 import com.diac.ydeas.domain.exception.ResourceNotFoundException;
+import com.diac.ydeas.domain.exception.ResourceOwnershipViolationException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.io.IOException;
 import java.util.HashMap;
@@ -16,7 +17,7 @@ import java.util.HashMap;
 /**
  * Глобальный обработчик исключений
  */
-@ControllerAdvice
+@RestControllerAdvice
 @RequiredArgsConstructor
 @Slf4j
 public class GlobalExceptionHandler {
@@ -68,6 +69,34 @@ public class GlobalExceptionHandler {
     ) throws IOException {
         log.warn(e.getMessage(), e);
         response.setStatus(HttpStatus.BAD_REQUEST.value());
+        response.setContentType("application/json");
+        response.getWriter().write(objectMapper.writeValueAsString(
+                new HashMap<>() {
+                    {
+                        put("message", e.getMessage());
+                        put("type", e.getClass());
+                    }
+                }
+        ));
+    }
+
+    /**
+     * Метод-обработчик исключений ResourceOwnershipViolationException
+     *
+     * @param e Объект-исключение
+     * @throws IOException В случае, если возникает ошибка записи в response
+     */
+    @ExceptionHandler(
+            value = {
+                    ResourceOwnershipViolationException.class
+            }
+    )
+    public void handleResourceOwnershipViolationException(
+            Exception e,
+            HttpServletResponse response
+    ) throws IOException {
+        log.warn(e.getMessage(), e);
+        response.setStatus(HttpStatus.FORBIDDEN.value());
         response.setContentType("application/json");
         response.getWriter().write(objectMapper.writeValueAsString(
                 new HashMap<>() {
