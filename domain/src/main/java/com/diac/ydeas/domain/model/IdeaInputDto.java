16f3b47d1d31f457320105eb8e2d242f47b7daed
diac <nikolai.gladkikh@gmail.com>
2023-08-09 16:43:48 +0300

    [#185785474] Domain: Add IdeaInputDto

---
+++ domain/src/main/java/com/diac/ydeas/domain/model/IdeaInputDto.java
@@ -0,0 +1,30 @@
+package com.diac.ydeas.domain.model;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * DTO для ввода идей в систему
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class IdeaInputDto {
+
+    /**
+     * Заголовок идеи
+     */
+    @NotNull(message = "Idea title is required")
+    @NotBlank(message = "Idea title cannot be blank")
+    private String title;
+
+    /**
+     * Описание идеи
+     */
+    @NotNull(message = "Idea description is required")
+    @NotBlank(message = "Idea description cannot be blank")
+    private String description;
+}
