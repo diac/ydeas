3aca8dd84a8b8da251ed4340ba82a61264dee10f
diac <nikolai.gladkikh@gmail.com>
2023-08-12 12:41:29 +0300

    [#185818680] Ideas module: Add pagination in the "Ideas rating" endpoint

--- ideas/src/main/java/com/diac/ydeas/ideas/controller/IdeaRatingController.java
+++ ideas/src/main/java/com/diac/ydeas/ideas/controller/IdeaRatingController.java
@@ -3,10 +3,13 @@ package com.diac.ydeas.ideas.controller;
 import com.diac.ydeas.domain.model.IdeaRating;
 import com.diac.ydeas.ideas.service.IdeaRatingService;
 import lombok.AllArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -19,6 +22,8 @@ import java.util.List;
 @RequestMapping("/idea_rating")
 public class IdeaRatingController {
 
+    private static final int RATINGS_PER_PAGE = 20;
+
     /**
      * Сервис для работы с объектами IdeaRating
      */
@@ -30,9 +35,11 @@ public class IdeaRatingController {
      * @return Тело ответа с рейтинговым списком идей и статусом OK
      */
     @GetMapping("")
-    public ResponseEntity<List<IdeaRating>> rating() {
+    public ResponseEntity<Page<IdeaRating>> rating(
+            @RequestParam(name = "page", required = false, defaultValue = "1") int pageNumber
+    ) {
         return new ResponseEntity<>(
-                ideaRatingService.rating(),
+                ideaRatingService.rating(PageRequest.of(pageNumber - 1, RATINGS_PER_PAGE)),
                 HttpStatus.OK
         );
     }
