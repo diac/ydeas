17fa8cde89770f648dbb1368fa2137e3b1f01f9c
diac <nikolai.gladkikh@gmail.com>
2023-08-14 13:00:21 +0300

    [#185784064] Domain: Add NotificationStatus enumeration

---
+++ domain/src/main/java/com/diac/ydeas/domain/enumeration/NotificationStatus.java
@@ -0,0 +1,22 @@
+package com.diac.ydeas.domain.enumeration;
+
+/**
+ * Перечисление статусов уведомлений
+ */
+public enum NotificationStatus {
+
+    /**
+     * Создано
+     */
+    CREATED,
+
+    /**
+     * Отправлено
+     */
+    SENT,
+
+    /**
+     * Ошибка отправки
+     */
+    FAILED
+}
