14affaeb94f8b3d72c7e36306f8429d38fed4081
diac <nikolai.gladkikh@gmail.com>
2023-08-23 13:06:14 +0300

    [#185785976] Add Javadoc in UserController

--- users/src/main/java/com/diac/ydeas/users/controllers/UserController.java
+++ users/src/main/java/com/diac/ydeas/users/controllers/UserController.java
@@ -9,13 +9,24 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+/**
+ * Контроллер, реализующий доступ к объектам модели User
+ */
 @RestController
 @RequestMapping("")
 @AllArgsConstructor
 public class UserController {
 
+    /**
+     * Сервис для работы с объектами User
+     */
     private final UserService userService;
 
+    /**
+     * Получить список всех пользователей
+     *
+     * @return Тело ответа со списком пользователей
+     */
     @RequestMapping("")
     public ResponseEntity<List<User>> findAll() {
         return ResponseEntity.ok()
