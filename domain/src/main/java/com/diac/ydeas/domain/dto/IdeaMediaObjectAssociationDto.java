233e72ec41ff379d1809172e5db8c6214acbc662
diac <nikolai.gladkikh@gmail.com>
2023-10-16 13:08:56 +0300

    [#185819901] Domain module: Add Swagger documentation in IdeaMediaObjectAssociationDto

--- domain/src/main/java/com/diac/ydeas/domain/dto/IdeaMediaObjectAssociationDto.java
+++ domain/src/main/java/com/diac/ydeas/domain/dto/IdeaMediaObjectAssociationDto.java
@@ -1,10 +1,16 @@
 package com.diac.ydeas.domain.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
 /**
  * DTO для определения связи между идеей и медиа-объектом
  *
  * @param ideaId        Идентификатор идеи
  * @param mediaObjectId Идентификатор медиа-объекта
  */
-public record IdeaMediaObjectAssociationDto(int ideaId, int mediaObjectId) {
+@Schema(description = "DTO для определения связи между идеей и медиа-объектом")
+public record IdeaMediaObjectAssociationDto(
+        @Schema(description = "Идентификатор идеи", example = "1") int ideaId,
+        @Schema(description = "Идентификатор медиа-объекта", example = "1") int mediaObjectId
+) {
 }
