d3adb3d4c0b679914b78c4e0463d602c1251298e
diac <nikolai.gladkikh@gmail.com>
2023-10-16 13:11:08 +0300

    [#185819901] Domain module: Add Swagger documentation in IdeaReviewNotificationDto

--- domain/src/main/java/com/diac/ydeas/domain/dto/IdeaReviewNotificationDto.java
+++ domain/src/main/java/com/diac/ydeas/domain/dto/IdeaReviewNotificationDto.java
@@ -1,6 +1,7 @@
 package com.diac.ydeas.domain.dto;
 
 import com.diac.ydeas.domain.enumeration.IdeaStatus;
+import io.swagger.v3.oas.annotations.media.Schema;
 
 import java.util.UUID;
 
@@ -12,10 +13,11 @@ import java.util.UUID;
  * @param ideaAuthorUuid UUID автора идеи
  * @param ideaStatus     Статус идеи после рассмотрения
  */
+@Schema(description = "DTO для передачи данных о рассмотрении идеи")
 public record IdeaReviewNotificationDto(
-        int ideaId,
-        String ideaTitle,
-        UUID ideaAuthorUuid,
-        IdeaStatus ideaStatus
+        @Schema(description = "Идентификатор идеи", example = "1") int ideaId,
+        @Schema(description = "Заголовок идеи", example = "My Idea") String ideaTitle,
+        @Schema(description = "UUID автора идеи", example = "550e8400-e29b-41d4-a716-446655440000") UUID ideaAuthorUuid,
+        @Schema(description = "Статус идеи после рассмотрения", example = "APPROVED") IdeaStatus ideaStatus
 ) {
 }
