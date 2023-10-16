8cb91aa10cf61352c7376db560589079922c6e53
diac <nikolai.gladkikh@gmail.com>
2023-10-16 13:13:29 +0300

    [#185819901] Domain module: Add Swagger documentation in StoredObjectDetailsDto

--- domain/src/main/java/com/diac/ydeas/domain/dto/StoredObjectDetailsDto.java
+++ domain/src/main/java/com/diac/ydeas/domain/dto/StoredObjectDetailsDto.java
@@ -1,5 +1,7 @@
 package com.diac.ydeas.domain.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
 /**
  * DTO с деталями хранимого объекта
  *
@@ -8,11 +10,12 @@ package com.diac.ydeas.domain.dto;
  * @param mediaType Тип медиа
  * @param contentLength Длина контента объекта
  */
+@Schema(description = "DTO с деталями хранимого объекта")
 public record StoredObjectDetailsDto(
-        String bucketName,
-        String objectKey,
-        String mediaType,
-        long contentLength
+        @Schema(description = "Имя бакета", example = "my-bucket") String bucketName,
+        @Schema(description = "Ключ объекта", example = "object-key") String objectKey,
+        @Schema(description = "Тип медиа", example = "image/jpeg") String mediaType,
+        @Schema(description = "Длина контента объекта", example = "1234") long contentLength
 ) {
 
 }
