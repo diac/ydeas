454c1fa3876a5e622ef723f65da22b0210f0a2f1
diac <nikolai.gladkikh@gmail.com>
2023-10-16 13:05:56 +0300

    [#185819901] Domain module: Add Swagger documentation in FileDownloadResultDto

--- domain/src/main/java/com/diac/ydeas/domain/dto/FileDownloadResultDto.java
+++ domain/src/main/java/com/diac/ydeas/domain/dto/FileDownloadResultDto.java
@@ -1,5 +1,6 @@
 package com.diac.ydeas.domain.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import org.springframework.core.io.Resource;
 
 /**
@@ -8,5 +9,9 @@ import org.springframework.core.io.Resource;
  * @param resource Ресурс с содержимым файла
  * @param mediaType Тип медиа файла
  */
-public record FileDownloadResultDto(Resource resource, String mediaType) {
+@Schema(description = "DTO с результатом скачивания файла")
+public record FileDownloadResultDto(
+        @Schema(description = "Ресурс с содержимым файла") Resource resource,
+        @Schema(description = "Тип медиа файла") String mediaType
+) {
 }
